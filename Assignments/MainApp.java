48df5743b15dfe88890b85144a3860fa67f37884
Kunaal28 <kunaal28401@gmail.com>
2024-03-27 14:14:17 +0530

    new Files

---
+++ Assignments/MainApp.java
@@ -0,0 +1,9 @@
+package assignment1;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+//         Quadrilateral quad = new Rectangle();
+	}
+
+}
---
+++ Assignments/Quadrilateral.java
@@ -0,0 +1,23 @@
+package assignment1;
+
+public class Quadrilateral {
+     int base;
+     int height;
+	public int getBase() {
+		return base;
+	}
+	public void setBase(int base) {
+		this.base = base;
+	}
+	public int getHeight() {
+		return height;
+		
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public static void main(String[] args) {
+            
+	}
+
+}
